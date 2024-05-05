3c5d8fc27011733887982f23d8e7f239773471e3
Vividh Choudhary <126601606+vividh07@users.noreply.github.com>
2024-05-05 12:35:07 +0000

    datatypes

---
+++ basics/dataTypes.js
@@ -0,0 +1,14 @@
+let a = "hello"
+console.log(typeof(a))   //string
+
+let b = 12
+console.log(typeof(b))  // number
+
+let c = true ;
+console.log(typeof(c))    // boolean
+
+let d = null
+console.log(typeof(d))   // object
+
+let e ;
+console.log(typeof(e))   // undefined
