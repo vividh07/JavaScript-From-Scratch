07a4f3711c181da2f7cffc1095a9fddcc034619e
Vividh Choudhary <126601606+vividh07@users.noreply.github.com>
2024-05-05 06:50:45 +0000

    variables

---
+++ basics/variables.js
@@ -0,0 +1,20 @@
+const accountId = 1234;
+// accountId = 1233444;
+ // const value cannot be changed 
+console.log(accountId)
+
+
+let a = 12
+{
+
+console.log(a)
+}
+console.log(a)
+ 
+// Let variable are global variables means their values work globally or out of their scope
+
+ {
+    var b = 123
+    console.log(b)
+// var values work in their scope only they are not global variables
+ }
