cb22594bb76a419f16d474737e6814458534e015
Vividh Choudhary <126601606+vividh07@users.noreply.github.com>
2024-05-06 17:36:07 +0000

    compariosin in js

---
+++ basics/comparision.js
@@ -0,0 +1,10 @@
+ console.log("2">1)
+
+ console.log(null>0)
+ console.log(null==0)
+ console.log(null>=0)
+
+ console.log(undefined==0)
+
+ console.log("2"==2)   // this checks value only 
+ console.log("2"===2)  // this checks value and data type both
