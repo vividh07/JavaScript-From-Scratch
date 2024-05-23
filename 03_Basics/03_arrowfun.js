36867eea959c24480755d422e6580e9ad2c2d88c
Vividh Choudhary <126601606+vividh07@users.noreply.github.com>
2024-05-23 13:14:43 +0000

    arrow function

--- 03_Basics/03_arrowfun.js
+++ 03_Basics/03_arrowfun.js
@@ -3,6 +3,20 @@
 const user = {
     userName : "xyz",
     price : 1000,
+   
+    welcomeMessage :function(){
+        console.log(`${this.userName}`)   //this refers to the current contxt or current function
 
+    }
   
 }
+user.welcomeMessage()
+
+// arrow function
+
+const abc = ()=>{
+    name : "nvc"
+    age : 12
+}
+
+abc()
