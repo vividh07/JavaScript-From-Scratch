96d539f5f231fd797e7d72d498a4e05f540ea41a
Vividh Choudhary <126601606+vividh07@users.noreply.github.com>
2024-05-24 13:01:05 +0000

    minor change

--- 03_Basics/04_iife.js
+++ 03_Basics/04_iife.js
@@ -1,11 +1,11 @@
 // immediately invoked functions expression
-
+// named IIFE
  (function abc(){
     console.log('hello')
  })(); 
 
  // to pass something in IIFE arrow function
-
+// unamed IIFE
  ((name)=>{
     console.log(`hello ${name}`)
  })('amit')
