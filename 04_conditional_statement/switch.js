a0ab23a9aea8b6418d11fe7098f25124a16c6f35
Vividh Choudhary <126601606+vividh07@users.noreply.github.com>
2024-05-25 13:51:19 +0000

    switch condition

---
+++ 04_conditional_statement/switch.js
@@ -0,0 +1,24 @@
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("Jan")
+        
+        break;
+    case 2:
+        console.log("Feb")
+        
+        break;
+    case 3:
+        console.log("Mar")
+        
+        break;
+    case 4:
+        console.log("Apr")
+        
+        break;
+
+    default:
+        console.log("no value")
+        break;
+}
