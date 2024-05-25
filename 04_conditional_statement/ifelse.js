1a4507fb2708af554e8baaa813f032c923306205
Vividh Choudhary <126601606+vividh07@users.noreply.github.com>
2024-05-25 13:40:01 +0000

    if-else condition

--- 04_conditional_statement/ifelse.js
+++ 04_conditional_statement/ifelse.js
@@ -4,3 +4,13 @@ const points = 500
 if(points >= 100){
   console.log("user have power to fly")
 }
+
+// if else condition
+
+const money = 500 
+if(money > 1000){
+  console.log("you are rich");
+}
+else{
+  console.log("you are poor");
+}
