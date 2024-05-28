173539039dc18c20f4a54ab1b1100fb278cd841c
Vividh Choudhary <126601606+vividh07@users.noreply.github.com>
2024-05-28 13:23:45 +0000

    if condition in for loop

--- 05_loops/forloop.js
+++ 05_loops/forloop.js
@@ -2,5 +2,24 @@
 
 
 for(var i =0; i <10;i++){
-    console.log(i)
+    const val = i;
+    console.log("*")
 }
+
+// console.log(val);  // we cant access this because this it out of the scope
+
+// if in for loop
+
+for(var a= 0; a<10; a++){
+    if(a==5){
+        console.log("5 is a best number")
+    }
+    console.log(a);
+
+}
+
+// // loop inside a loop 
+
+// for(i=0;i<10;i++){
+//     for()
+// }
