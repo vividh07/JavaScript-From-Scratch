537e5c652f3033ff68962d91f6587609812d8f0a
Vividh Choudhary <126601606+vividh07@users.noreply.github.com>
2024-06-18 13:15:23 +0000

    map

--- 05_loops/forof.js
+++ 05_loops/forof.js
@@ -2,12 +2,33 @@ let heros = ["batman","superman","ironman"]
 
 
 for(const hero of heros){
-    console.log(hero)
+    //console.log(hero)
 }
 
 
 let greet = "hello wolrd"
 
 for(greetings of greet){
-    console.log(greetings)
+    //console.log(greetings)
+}
+
+
+// maps
+
+const map = new Map()
+map.set('IN',"Inida")
+map.set('USA',"united states of america")
+map.set('FR',"france")
+map.set('IN',"Inida")             // in map values are unique sp this will not print 
+//console.log(map)
+
+for(const key of map){
+    console.log(key)
+}
+
+//destructure of an array
+
+for (const [key,value] of map) {
+    console.log(key)
+    
 }
