dee9794b5b5ae764564a2daf112f7d93e640ff33
Vividh Choudhary <126601606+vividh07@users.noreply.github.com>
2024-06-18 13:00:22 +0000

    for of loop

---
+++ 05_loops/forof.js
@@ -0,0 +1,13 @@
+let heros = ["batman","superman","ironman"]
+
+
+for(const hero of heros){
+    console.log(hero)
+}
+
+
+let greet = "hello wolrd"
+
+for(greetings of greet){
+    console.log(greetings)
+}
